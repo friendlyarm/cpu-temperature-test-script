813bece9b6f1050d6f342634c36adb5cb15e770e
Lawrence-Tang <tangzongsheng@gmail.com>
2020-12-14 18:06:25 +0800

    add big-cpu-core's freq

--- capture_cpufreq_and_temp.sh
+++ capture_cpufreq_and_temp.sh
@@ -11,8 +11,11 @@ done
 ps | grep dd > /tmp/cpufan_testing_started
 for i in {1..1800}
 do
-    awk '{printf ("%0.0f",$1/1000); }' </sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_cur_freq >> /tmp/cpufan_testing_cpu-freq.txt
-    awk '{printf ("%0.1f",$1/1000); }' </sys/class/thermal/thermal_zone0/temp >> /tmp/cpufan_testing_cpu-temperature.txt
+    awk '{printf ("%0.0f\n",$1/1000); }' </sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_cur_freq >> /tmp/cpufan_testing_cpu-freq.txt
+    if [ -f /sys/devices/system/cpu/cpu4/cpufreq/cpuinfo_cur_freq ]; then
+        awk '{printf ("%0.0f\n",$1/1000); }' </sys/devices/system/cpu/cpu4/cpufreq/cpuinfo_cur_freq >> /tmp/cpufan_testing_cpu4-freq.txt
+    fi
+    awk '{printf ("%0.1f\n",$1/1000); }' </sys/class/thermal/thermal_zone0/temp >> /tmp/cpufan_testing_cpu-temperature.txt
     sleep 1
 done
 ps | grep dd > /tmp/cpufan_testing_done
